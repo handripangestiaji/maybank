6003e8bc2510f99e116c75e02a627751f618f555
Eko Purnomo <eko.purnomo@icloud.com>
2013-11-13 13:38:48 +0700

    + change access_token

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,6 +1,6 @@
 <?php
 
-$config['fb_appid'] = "241368255964045";
-$config['fb_appsecret'] = "4aa13ecd84b1033b95a2b1bfd14190f6";
+$config['fb_appid'] = "394562987342152";
+$config['fb_appsecret'] = "2d4e868f45c5c5ff6288399f214a5a23";
 $config['dummy_access_token'] = 'CAACEdEose0cBAHpoqvmkjPItt3NWfcWzrvYZBJlsZCD5eCgTGDHFzxqguiXbwZAN26GnMwuOLisKQ5Bfo3o94uH5bsCtCZAdla7yD17CVCua45LlXwvsAZBKODmy1sT2nfjobcUB3tY2xy4qQWr1bZA7XsrDjZB2QZA4i2zNViMjzzz8G3tqWmaZAhZCVxPlTUUeq29HcbtgA6dwZDZD';
 

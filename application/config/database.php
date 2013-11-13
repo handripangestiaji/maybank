fe4c35e7e78b30414ad7b2c4f3378f2d398c9fd9
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2013-11-13 11:40:10 +0700

    reconfigured database config for staging server

--- application/config/database.php
+++ application/config/database.php
@@ -50,8 +50,8 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
+$db['default']['password'] = 'y0lk3r@123@mysql';
+$db['default']['database'] = 'ion_auth';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;

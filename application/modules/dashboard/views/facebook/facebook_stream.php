6ccfa861f9958e34d320ae91232c1b571a87176b
bena <benawv@gmail.com>
2013-11-15 09:26:01 +0700

    bena -> facebook stream

--- application/modules/dashboard/views/facebook/facebook_stream.php
+++ application/modules/dashboard/views/facebook/facebook_stream.php
@@ -46,17 +46,17 @@
                     <div class="container-fluid">
                         <!-- ==================== ALL ACTIVITIES CONTENT ==================== -->
                         <ul class="floatingBoxContainers" id="allActivities">
-                            <?php //$this->load->view('dashboard/facebook/wall_post', array('fb_feed' => $fb_feed))?> 
+                            <?php $this->load->view('dashboard/facebook/wall_post', array('fb_feed' => $fb_feed))?> 
                         </ul>
                         <div class="filled" style="text-align: center;"><button class="btn btn-info"><i class="icon-chevron-down"></i> LOAD MORE</button></div>
                         <!-- ==================== END OF ALL ACTIVITIES CONTENT ==================== -->
 
                         <!-- ==================== RECENT COMMENTS CONTENT ==================== -->
                         <ul class="floatingBoxContainers" id="recentComments" style="display:none">
-                            <?php //$this->load->view('dashboard/facebook/private_message', array('fb_feed' => $fb_feed))?> 
+                            <?php $this->load->view('dashboard/facebook/private_message', array('fb_feed' => $fb_feed))?> 
                         </ul>
                         <ul class="floatingBoxContainers" id="maybankpost" style="display:none">
-                            <?php //$this->load->view('dashboard/facebook/own_post', array('own_post' => $own_post))?> 
+                            <?php $this->load->view('dashboard/facebook/own_post', array('own_post' => $own_post))?> 
                         </ul>
                         <!-- ==================== END OF RECENT COMMENTS CONTENT ==================== -->
                     </div>

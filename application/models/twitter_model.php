acea46d429a4e68a56fc21946c13da0c5a17d928
Eko Purnomo <eko.purnomo@icloud.com>
2013-11-18 23:37:59 +0700

    eko: twitter model…

--- application/models/twitter_model.php
+++ application/models/twitter_model.php
@@ -15,19 +15,146 @@ class twitter_model extends CI_Model
         $this->connection = $this->twitteroauth->create("EXWkuGJlJ8zMUGccF04uMA","eUw0K1YuzMof6RqI0oYw22mV6JYH0UbePMscMSiDZk",$access_token,$access_secret);
     }
     
-    public function Mentions(){
-        
+    public function Mentions($channel){
+        $result = $this->connection->get('statuses/mentions_timeline',
+                                         array("count" => 200));
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
+        foreach($result as $tweet){
+            $this->SaveTwitterUsers($tweet->user);
+            $this->SaveTweets($tweet, $channel, "mentions");
+        }
     }
     
     public function DirectMessages(){
         
     }
     
-    public function OwnPost(){
+    public function OwnPost($channel){
+        $result = $this->connection->get('statuses/user_timeline',
+                    array(
+                        "user_id" => $channel->social_id
+                    ));
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
+        if(is_array($result)){
+            foreach($result as $tweet){
+                $this->SaveTwitterUsers($tweet->user);
+                $this->SaveTweets($tweet, $channel, "user_timeline");
+            }
+        }
+    }
+    
+    public function HomeFeed($channel){
+        $result = $this->connection->get('statuses/home_timeline',
+                    array(
+                        "user_id" => $channel->social_id
+                    ));
+        echo "<pre>";
+        print_r($result);
+        echo "</pre>";
+        if(is_array($result)){
+            foreach($result as $tweet){
+                $this->SaveTwitterUsers($tweet->user);
+                $this->SaveTweets($tweet, $channel, "home_feed");
+            }
+        }
+    }
+    
+    public function SaveTweets($tweet, $channel, $come_from = "mentions"){
+        $this->db->trans_start();
+        $timezone = new DateTimeZone("Europe/London");
+        $created_at = new DateTime($tweet->created_at, $timezone);
+        $retrieved_at = new DateTime(date("Y-m-d H:i:s e"), $timezone);
+        $post_id = $this->GetTweetId($tweet->id_str);
+        $social_stream = array(
+	    "post_stream_id" => $tweet->id_str,
+	    "channel_id" => $channel->channel_id,
+	    "type" => "twitter",
+	    "retrieved_at" => $retrieved_at->format("Y-m-d H:i:s e"),
+	    "created_at" => $created_at->format("Y-m-d H:i:s")
+	);
+        
+        $tweet_to_save = array(
+            "type" => $come_from,
+            "favorited" =>  $tweet->favorited,
+            "in_reply_to" => $post_id == null ? NULL : $post_id->post_id,
+            "twitter_entities" => $tweet->entities == "" ? "" : json_encode($tweet->entities),
+            "text" => $tweet->text,
+            "retweet_count" => $tweet->retweet_count,
+            "geolocation" => json_encode($tweet->place),
+            "source" => $tweet->source,
+            "twitter_user_id" => $tweet->user->id_str,
+            "created_at" => $created_at->format("Y-m-d H:i:s")
+        );
+        
+        if($post_id == null){
+            $this->db->insert("social_stream", $social_stream);
+            $post_id = $this->db->insert_id();
+            $tweet_to_save['post_id'] = $post_id;
+            $this->db->insert("social_stream_twitter", $tweet_to_save);
+        }
+        else{
+            $this->db->where("post_id", $post_id->post_id);
+            $this->db->update("social_stream", $social_stream);
+            $this->db->where("post_id", $post_id->post_id);
+            $this->db->update("social_stream_twitter", $tweet_to_save);
+        }
+        $this->db->trans_complete();
+    }
+    
+    public function SaveDirectMessages(){
         
     }
     
-    public function HomeFeed(){
+    public function SaveTwitterUsers($user){
+        $timezone = new DateTimeZone("Europe/London");
+        $created_at = new DateTime($user->created_at, $timezone);
+        $retrieved_at = new DateTime(date("Y-m-d H:i:s e"), $timezone);
+        $user_to_save = array(
+            "twitter_user_id" => $user->id_str,
+            "screen_name" => $user->screen_name,
+            "profile_image_url" => $user->profile_image_url_https,
+            "name" => $user->name,
+            "url" => $user->url,
+            "description" => $user->description,
+            "location" => $user->location,
+            "statuses_count" => $user->statuses_count,
+            "friends_count" => $user->friends_count,
+            "followers_count" => $user->followers_count,
+            "verified_account" => $user->verified, 
+            "time_zone" => $user->time_zone,
+            "following" => $user->following,
+            "created_at" => $created_at->format("Y-m-d H:i:s") ,
+            "retrieved_at" => $retrieved_at->format("Y-m-d H:i:s")
+        );
         
+        if($this->GetTwitterUsers($user->id_str) == null){
+            $this->db->insert("twitter_user_engaged", $user_to_save);
+        }
+        else{
+            $this->db->where("twitter_user_id", $user->id_str);
+            unset($user_to_save['twitter_user_id']);
+            $this->db->update("twitter_user_engaged", $user_to_save);
+        }
+    }
+    
+    public function GetTwitterUsers($id){
+        $this->db->select("twitter_user_id");
+        $this->db->where("twitter_user_id", $id);
+        $this->db->from("twitter_user_engaged");
+        return $this->db->get()->row();
+    }
+    
+    public function GetTweetId($post_stream_id){
+        $this->db->select("post_id");
+        $this->db->from("social_stream");
+        $this->db->where(array(
+                               "post_stream_id" => $post_stream_id,
+                               "type" => "twitter"
+                        ));
+        return $this->db->get()->row();
     }
 }

26d697678b2f72921d4cfcb0f7ed2eacd180ed7b
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2013-11-25 13:27:51 +0700

    Shorturl update

--- application/models/shorturl_model.php
+++ application/models/shorturl_model.php
@@ -10,7 +10,7 @@ class Shorturl_model extends CI_Model
 		parent::__construct();
 	}
 	
-	public function getOneBy($params = array())
+	public function find($params = array())
 	{
 		if (isset($params['long_url']))
 		{

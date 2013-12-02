ce10df18d298adaff8b159caf1e6c92524e6b738
bena <benawv@gmail.com>
2013-12-02 16:28:44 +0700

    bena -> jquery widget tree checkbox role users

--- application/views/metadata.php
+++ application/views/metadata.php
@@ -5,7 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="stylesheet" href="<?php echo base_url()?>media/css/bootstrap-responsive.min.css">
 <link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
-
+<link rel="stylesheet" href="<?php echo base_url(); ?>media/js/vendor/jqwidgets/styles/jqx.base.css"/> <!-- jquery widget tree -->
 <link rel="stylesheet" href="<?php echo base_url()?>media/css/styles.css">
 <script type="text/javascript">
     var BASEURL = "<?=base_url()?>";

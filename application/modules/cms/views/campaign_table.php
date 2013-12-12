ee6e0ce5c65724727e1a5e312d27343de402ee78
Handri Pangestiaji <handri.pangestiaji@gmail.com>
2013-12-12 23:08:35 +0700

    campaign and shorturl model update

--- application/modules/cms/views/campaign_table.php
+++ application/modules/cms/views/campaign_table.php
@@ -28,7 +28,7 @@
 	                    <div class="pull-left" style="width: 70%; color: #FFFFFF;">
 	                        <p><span style="font-weight: bold">TAG: <?php echo implode(" , ",$v['tag_name']); ?></span></p>
 	                        <p><span style="font-weight: bold">Description:</span> <?php echo $v['description']?></p>
-	                        <p><span style="font-weight: bold">Total Clicks:</span> 123 Clicks &nbsp;&nbsp;&nbsp; <span style="font-weight: bold">Total URL Created:</span> 5</p>
+	                        <p><span style="font-weight: bold">Total Clicks:</span> <?php echo $v['total_clicks']?> Clicks &nbsp;&nbsp;&nbsp; <span style="font-weight: bold">Total URL Created:</span> <?php echo count($v['short_urls']) ?></p>
 	                    </div>
 	                    <div class="pull-right" style="width: 25%; text-align: right">
 	                        <p>

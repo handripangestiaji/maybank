fe04e45449e4ac26f027c4e4190c942b0e28840f
bena <benawv@gmail.com>
2013-12-17 16:19:29 +0700

    bena -> short url campaign tab

--- application/modules/cms/views/create_short_url.php
+++ application/modules/cms/views/create_short_url.php
@@ -2,70 +2,153 @@
     <h4>Create Short URL</h4>    
 </div>
  <div class="floatingBox">
-    <div class="container-fluid campaignForm">
-        <form class="form-horizontal contentForm" method="post" action="<?php echo site_url('cms/create_short_url')?>">
-            <div class="control-group">
-                <label class="control-label">Full URL Path</label>
-                <div class="controls">
-                  <input type="text" class="span10" name="shorturl[long_url]" placeholder="http://www.maybank2u.com.my/">
-                  <?php echo "<br />".$this->session->userdata('message')?>
+                    <!-- ==================== TAB ROW ==================== -->
+                <div class="row-fluid">
+                    <div class="span12 tabContainer">
+                        <!-- ==================== TAB NAVIGATION ==================== -->
+                        <ul class="nav nav-tabs">
+                            <li class="active">
+                                <a href="#firstTab">Campaign</a>
+                            </li>
+                            <li><a href="#secondTab">Non Campaign</a></li>
+                        </ul>
+                        <!-- ==================== END OF TAB NAVIGATIION ==================== -->
+
+                        <div class="container-fluid">
+                            <!-- ==================== FIRST TAB CONTENT ==================== -->
+                            <div class="tabContent" id="firstTab">
+                               <div class="floatingBox span12">
+                                    <div class="container-fluid campaignForm">
+                                        <form class="form-horizontal contentForm" method="post" action="<?php echo site_url('cms/create_short_url')?>">
+                                            <div class="control-group">
+                                                <label class="control-label">Full URL Path</label>
+                                                <div class="controls">
+                                                  <input type="text" class="span10" name="shorturl[long_url]" placeholder="http://www.maybank2u.com.my/">
+                                                  <?php echo "<br />".$this->session->userdata('message')?>
+                                                </div>
+                                            </div>
+                                            <div class="control-group">
+                                                <label class="control-label">Description</label>
+                                                <div class="controls">
+                                                  <textarea class="span10" name="shorturl[description]"></textarea>
+                                                </div>
+                                            </div>
+                                            <div class="control-group">
+                                                <label class="control-label">Campaign</label>
+                                                <div class="controls">
+                                                    <select id="uniqueSelect" name="shorturl[campaign_id]">
+                                                        <?php if($campaigns): ?>
+                                                                <?php $i=1;?>
+                                                                <?php foreach($campaigns as $v): ?>
+                                                                        <option id="opt<?php echo $i?>" value="<?php echo $v->id?>"><?php echo $v->campaign_name ?></option>
+                                                                <?php endforeach; ?>
+                                                        <?php endif; ?>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <!--
+                                            <div class="control-group">
+                                                <label class="control-label">Product</label>
+                                                <div class="controls">
+                                                    <select id="multipleSelect" multiple="multiple">
+                                                        <option value="opt7">First Option</option>
+                                                        <option value="opt8">Second Option</option>
+                                                        <option value="opt9">Third Option</option>
+                                                        <option value="opt10">Fourth Option</option>
+                                                        <option value="opt11">Fifth Option</option>
+                                                        <option value="opt12">Sixth Option</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="control-group">
+                                                <label class="control-label">Tag</label>
+                                                <div class="controls">
+                                                  <input type="text" class="span10">
+                                                </div>
+                                            </div>
+                                            -->
+                                            <div class="control-group">
+                                                <label class="control-label">Short URL</label>
+                                                <div class="controls">
+                                                  http://maybk.co/<input type="text" class="span10" name="shorturl[short_code]" style="width: 100px;" value="<?php echo $code?>" maxlength="6"/>
+                                            </div>
+                                            <div class="control-group">
+                                                <div class="pull-left">
+                                                    <button class="btn btn-primary" type="submit">Create</button>
+                                                </div>
+                                                <div class="pull-right">
+                                                    <button class="btn " type="button">Cancel</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    </div>
+                               </div>
+                            </div>
+                            <!-- ==================== END OF FIRST TAB CONTENT ==================== -->
+
+                            <!-- ==================== SECOND TAB CONTENT ==================== -->
+                            <div class="tabContent" id="secondTab" style="display: none">
+                                <div class="floatingBox span12">
+                                    <div class="container-fluid campaignForm">
+                                        <form class="form-horizontal contentForm" method="post" action="<?php echo site_url('cms/create_short_url')?>">
+                                            <div class="control-group">
+                                                <label class="control-label">Full URL Path</label>
+                                                <div class="controls">
+                                                  <input type="text" class="span10" name="shorturl[long_url]" placeholder="http://www.maybank2u.com.my/">
+                                                  <?php echo "<br />".$this->session->userdata('message')?>
+                                                </div>
+                                            </div>
+                                            <div class="control-group">
+                                                <label class="control-label">Description</label>
+                                                <div class="controls">
+                                                  <textarea class="span10" name="shorturl[description]"></textarea>
+                                                </div>
+                                            </div>
+                                            <!--
+                                            <div class="control-group">
+                                                <label class="control-label">Product</label>
+                                                <div class="controls">
+                                                    <select id="multipleSelect" multiple="multiple">
+                                                        <option value="opt7">First Option</option>
+                                                        <option value="opt8">Second Option</option>
+                                                        <option value="opt9">Third Option</option>
+                                                        <option value="opt10">Fourth Option</option>
+                                                        <option value="opt11">Fifth Option</option>
+                                                        <option value="opt12">Sixth Option</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                            <div class="control-group">
+                                                <label class="control-label">Tag</label>
+                                                <div class="controls">
+                                                  <input type="text" class="span10">
+                                                </div>
+                                            </div>
+                                            -->
+                                            <div class="control-group">
+                                                <label class="control-label">Short URL</label>
+                                                <div class="controls">
+                                                  http://maybk.co/<input type="text" class="span10" name="shorturl[short_code]" style="width: 100px;" value="<?php echo $code?>" maxlength="6"/>
+                                            </div>
+                                            <div class="control-group">
+                                                <div class="pull-left">
+                                                    <button class="btn btn-primary" type="submit">Create</button>
+                                                </div>
+                                                <div class="pull-right">
+                                                    <button class="btn " type="button">Cancel</button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                    </div>
+                            </div>
+                            <!-- ==================== END OF SECOND TAB CONTENT ==================== -->
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="control-group">
-                <label class="control-label">Description</label>
-                <div class="controls">
-                  <textarea class="span10" name="shorturl[description]"></textarea>
                 </div>
-            </div>
-            <div class="control-group">
-                <label class="control-label">Campaign</label>
-                <div class="controls">
-                    <select id="uniqueSelect" name="shorturl[campaign_id]">
-                    	<?php if($campaigns): ?>
-                    		<?php $i=1;?>
-                    		<?php foreach($campaigns as $v): ?>
-                    			<option id="opt<?php echo $i?>" value="<?php echo $v->id?>"><?php echo $v->campaign_name ?></option>
-                    		<?php endforeach; ?>
-                    	<?php endif; ?>
-                    </select>
-                </div>
-            </div>
-            <!--
-            <div class="control-group">
-                <label class="control-label">Product</label>
-                <div class="controls">
-                    <select id="multipleSelect" multiple="multiple">
-                        <option value="opt7">First Option</option>
-                        <option value="opt8">Second Option</option>
-                        <option value="opt9">Third Option</option>
-                        <option value="opt10">Fourth Option</option>
-                        <option value="opt11">Fifth Option</option>
-                        <option value="opt12">Sixth Option</option>
-                    </select>
-                </div>
-            </div>
-            <div class="control-group">
-                <label class="control-label">Tag</label>
-                <div class="controls">
-                  <input type="text" class="span10">
-                </div>
-            </div>
-            -->
-            <div class="control-group">
-                <label class="control-label">Short URL</label>
-                <div class="controls">
-                  http://maybk.co/<input type="text" class="span10" name="shorturl[short_code]" style="width: 100px;" value="<?php echo $code?>" maxlength="6"/>
-            </div>
-            <div class="control-group">
-                <div class="pull-left">
-                    <button class="btn btn-primary" type="submit">Create</button>
-                </div>
-                <div class="pull-right">
-                    <button class="btn " type="button">Cancel</button>
-                </div>
-            </div>
-        </form>
-    </div>
+                <!-- ==================== END OF TAB ROW ==================== -->
 </div>
 <div class="row-fluid" style="border-bottom: solid 1px #C9C9C9; margin-bottom: 10px;">
     <h4>Short URL List</h4>    

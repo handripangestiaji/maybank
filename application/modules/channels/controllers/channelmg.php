c717a8eb43f9ba79efa20911de4c029addc50c3d
Eko Purnomo <eko.purnomo@icloud.com>
2013-12-26 10:16:31 +0700

    Eko: Youtube channel

--- application/modules/channels/controllers/channelmg.php
+++ application/modules/channels/controllers/channelmg.php
@@ -124,7 +124,80 @@ class ChannelMg extends MY_Controller {
         //redirect('channels/channelmg');
     }
     
+    public function AddYoutube(){
+        $this->load->config('youtube');
+        $youtube = $this->config->item('youtube');
+        $this->load->library('google_libs/Google_Client');
+        require_once './application/libraries/google_libs/contrib/Google_YouTubeService.php';
+        
+        $this->google_client->setClientId($youtube['client_id']);
+        $this->google_client->setClientSecret($youtube['client_secret']);
+        $redirect = filter_var(base_url('channels/channelmg/AddYoutube'), FILTER_SANITIZE_URL);
+        $this->google_client->setRedirectUri($redirect);
+        
+        $youtubeObject = new Google_YoutubeService($this->google_client);
+        
+        if($this->input->get('code')){
+            $this->google_client->authenticate();
+            $this->session->set_userdata('youtube_at', $this->google_client->getAccessToken());
+            $channel = array(
+                
+            );
+            
+        }else{
+            $state = mt_rand();
+            $this->google_client->setState($state);
+            $this->session->set_userdata('youtube_state', $state);
+          
+            $authUrl = $this->google_client->createAuthUrl();
+            redirect($authUrl);
+        }
+        
+        
+    }
     
+    public function Test(){
+        $this->load->config('youtube');
+        $youtube = $this->config->item('youtube');
+        $youtube_access_token = $this->session->userdata('youtube_at');
+        $this->load->library('google_libs/Google_Client');
+        require_once './application/libraries/google_libs/contrib/Google_YouTubeService.php';
+        print_r($youtube_access_token);
+        
+        $this->google_client->setClientId($youtube['client_id']);
+        $this->google_client->setClientSecret($youtube['client_secret']);
+        $redirect = filter_var(base_url('channels/channelmg/AddYoutube'), FILTER_SANITIZE_URL);
+        $this->google_client->setAccessToken($youtube_access_token);
+
+        $youtube_object = new Google_YouTubeService($this->google_client);
+        $channelsResponse = $youtube_object->channels->listChannels('contentDetails', array(
+            'mine' => 'true',
+          ));
+        
+        echo "<pre>";print_r($channelsResponse);
+        print_r($youtube_access_token);
+        foreach ($channelsResponse['items'] as $channel) {
+            // Extract the unique playlist ID that identifies the list of videos
+            // uploaded to the channel, and then call the playlistItems.list method
+            // to retrieve that list.
+            $uploadsListId = $channel['contentDetails']['relatedPlaylists']['uploads'];
+      
+            $playlistItemsResponse = $youtube_object->playlistItems->listPlaylistItems('snippet', array(
+              'playlistId' => $uploadsListId,
+              'maxResults' => 50
+            ));
+            
+            $htmlBody = "<h3>Videos in list $uploadsListId</h3><ul>";
+            foreach ($playlistItemsResponse['items'] as $playlistItem) {
+                
+                print_r($playlistItem);
+                
+            }
+        }
+        
+        echo "</pre>";
+    }
+
     
     
 }

bad13ea111ac8e256431213f4719fac6e38be41f
bena <benawv@gmail.com>
2014-02-20 17:01:33 +0700

    fix issues #86 #79 #67

--- application/models/campaign_model.php
+++ application/models/campaign_model.php
@@ -142,6 +142,7 @@ class Campaign_model extends CI_Model
 			{
 				$campaigns[$i]['short_urls'][$x]['long_url'] = $v->long_url;
 				$campaigns[$i]['short_urls'][$x]['short_code'] = $v->short_code;
+				$campaigns[$i]['short_urls'][$x]['description'] = $v->description;
 				$campaigns[$i]['short_urls'][$x]['increment'] = $v->increment;
 				$campaigns[$i]['short_urls'][$x]['created_at'] = date('M d, Y', strtotime($v->created_at));
 				$campaigns[$i]['short_urls'][$x]['display_name'] = $v->display_name;

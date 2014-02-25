050cff97a226af2fec45b7eb0733142675918159
Eko Purnomo <eko.purnomo@icloud.com>
2014-02-25 14:39:23 +0700

    Issue #89 #73

--- application/modules/dashboard/views/twitter/twitter_mentions.php
+++ application/modules/dashboard/views/twitter/twitter_mentions.php
@@ -45,7 +45,7 @@ for($i=0;$i<count($mentions);$i++){
     </p>
     <p class="indicator">
     <?php if($mentions[$i]->case_id):?>
-        <button type="button" class="btn <?=$mentions[$i]->case[0]->status == "pending" ? "btn-purple" : "btn-inverse"?> btn-mini" value="<?php echo $mentions[$i]->case_id?>">CASE ID #<?php echo $mentions[$i]->case_id?>
+        <button type="button" class="btn <?=$mentions[$i]->case[0]->status == "pending" ? "btn-purple" : "btn-inverse"?> btn-mini" value="<?php echo $mentions[$i]->case[0]->case_id?>">CASE ID #<?php echo $mentions[$i]->case[0]->case_id?>
             <?php
             if($mentions[$i]->case[0]->status == "pending")
                 echo isset($mentions[$i]->case[0]->assign_to->full_name) ? ' Assign to '.$mentions[$i]->case[0]->assign_to->full_name : '';

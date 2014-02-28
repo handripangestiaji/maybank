85afa328955e4da91b05d3cc063dad125d07778c
bena <benawv@gmail.com>
2014-02-28 18:07:08 +0700

    add parent_id and country_code to product

--- application/modules/cms/controllers/cms.php
+++ application/modules/cms/controllers/cms.php
@@ -426,7 +426,7 @@ class Cms extends MY_Controller {
     {
      if(IsRoleFriendlyNameExist($this->user_role,'Content Management_Product_View'))
      {
-    	$data['campaigns'] = '';
+        $data['campaigns'] = '';
     	
     	$data['tags'] = '';
     	
@@ -457,8 +457,11 @@ class Cms extends MY_Controller {
 		$offset = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
     	
     	$data['products'] = $this->product_model->get($config['per_page'], $offset);
-    	
-    	$data['pagination'] = $this->pagination->create_links();
+        
+          $data['products_avail'] = $this->product_model->get();
+    	  $data['countries'] = $this->users_model->get_country()->result();
+        
+        $data['pagination'] = $this->pagination->create_links();
     	
     	$action = $this->input->get('action');
         
@@ -521,6 +524,8 @@ class Cms extends MY_Controller {
     {
           $product = $this->product_model->getOneBy(array('id' => $id));
           $data['row'] = $product;
+          $data['products_avail'] = $this->product_model->get();
+    	  $data['countries'] = $this->users_model->get_country()->result();
           $this->load->view('cms/edit_product',$data);
     }
     
@@ -528,6 +533,8 @@ class Cms extends MY_Controller {
           $this->form_validation->set_rules('id','Id','required');
           $this->form_validation->set_rules('name','Product Name','required');
           $this->form_validation->set_rules('description','Description','required');
+          $this->form_validation->set_rules('parent_id','Parent','required');
+          $this->form_validation->set_rules('country_code','Country','required');
           
           if ($this->form_validation->run() == FALSE){
                $this->load->view('cms/edit_product');
@@ -535,7 +542,9 @@ class Cms extends MY_Controller {
           else{
                $id = $this->input->post('id');
                $value = array('product_name' => $this->input->post('name'),
-                              'description' => $this->input->post('description')
+                              'description' => $this->input->post('description'),
+                              'parent_id' => $this->input->post('parent_id'),
+                              'country_code' => $this->input->post('country_code')
                              );
                $result = $this->product_model->update($id,$value);
                $this->session->set_flashdata('msg','Product has been updated');
--- application/modules/cms/views/create_product.php
+++ application/modules/cms/views/create_product.php
@@ -21,6 +21,33 @@
                 </div>
             </div>
             <div class="control-group">
+                <label class="control-label">Parent</label>
+                <div class="controls">
+                <select name="product[parent_id]">
+                    <?php foreach($products_avail as $product){ ?>
+                        <option value="<?php echo $product->id ?>"><?php echo $product->product_name ?></option>
+                    <?php } ?>
+                    </select>
+                </div>
+            </div>
+            <?php if($this->session->userdata('country') == 'All'){ ?>
+            <div class="control-group">
+                <label class="control-label">Country</label>
+                <div class="controls">
+                    <select name="product[country_code]">
+                        <?php foreach($countries as $country){
+                            if($country->code != 'All'){ ?>
+                                <option value="<?php echo $country->code ?>"><?php echo $country->name ?></option>
+                            <?php } ?>
+                        <?php } ?>
+                    </select>
+                </div>
+            </div>
+            <?php }
+            else{ ?>
+                <input type="hidden" name="product[country_code]" value="<?php echo $this->session->userdata('country') ?>">
+            <?php }?>
+            <div class="control-group">
                 <div class="pull-left">
                     <button class="btn btn-primary" type="submit">Create</button>
                 </div>
@@ -64,8 +91,7 @@
 		                <td><?php echo $v->description; ?></td>
 		                <td><?php echo $v->increment; ?></td>
 		                <td><?php echo $v->display_name; ?></td>
-                                <?php print_r($this->user_role); ?>
-		                <?php for($i=0;$i<count($this->user_role);$i++){
+                                <?php for($i=0;$i<count($this->user_role);$i++){
 				    if($this->user_role[$i]->role_friendly_name=='Content Management_Product_Edit'){
 				    ?>
                                 <td>

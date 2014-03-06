89147636088c7b5d55b8b443374719872b65d9df
bena <benawv@gmail.com>
2014-03-06 14:16:41 +0700

    fix issue #110

--- application/modules/cms/views/index.php
+++ application/modules/cms/views/index.php
@@ -13,13 +13,19 @@
 <?php }
 }?>
 <div class="row-fluid" style="width: 100%; margin: 0px auto;">
-    <div class="row-fluid" >
+    <!--div class="row-fluid" >
         <div class="pull-left">
-            <!--input type="text" placeholder="Search" style="width:175px; float: left; margin-left: 2px;">
+            <input type="text" placeholder="Search" style="width:175px; float: left; margin-left: 2px;">
             <a href="" style="float: left; height: 14px;">
-                <span class="add-on" style="background-color: black;color: white;margin-left: -1px; display: inline-block; white-space: nowrap; padding: 5px 6px; font-size: 14px;"><i class="icon-search"></i></span></a!-->
+                <span class="add-on" style="background-color: black;color: white;margin-left: -1px; display: inline-block; white-space: nowrap; padding: 5px 6px; font-size: 14px;"><i class="icon-search"></i></span></a>
         </div>
         <div class="pull-right">
+            
+        </div>
+    </div!-->
+    <div class="cms-content row-fluid">
+        <div class="cms-filter pull-left">
+            <a href="<?php echo base_url('cms'); ?>"><button class="btn btn-inverse" type="button"><i class="icon-building"></i> View Campaigns & URL</button></a>
             <?php for($i=0;$i<count($this->user_role);$i++){?>
                 <?php if($this->user_role[$i]->role_friendly_name=='Content Management_Short_URL_View'){?>
             <a href="<?php echo base_url('cms/create_short_url'); ?>"><button class="btn btn-inverse" type="button"><i class="icon-code"></i> Create Short URL</button></a>
@@ -30,20 +36,6 @@
                 <?php } if($this->user_role[$i]->role_friendly_name=='Content Management_TAG_View'){?>
             <a href="<?php echo base_url('cms/create_tag'); ?>"><button class="btn btn-inverse" type="button"><i class="icon-tag"></i> Create Tag</button></a>
             <?php }}?>
-        </div>
-    </div>
-    <div class="cms-content row-fluid">
-        <div class="cms-filter pull-left">
-            <h5>Filter :</h5>
-                <select>
-                    <option value="user">Campaign</option>
-                </select>
-                <select>
-                    <option value="user">Product</option>
-                </select>
-                <select>
-                    <option value="user">Tag</option>
-                </select>
         </div>
         <div class="cms-table pull-right">
             <?php

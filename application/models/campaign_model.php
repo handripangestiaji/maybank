fc797470e7d34d4827d411cb2a072696720ef2ab
bena <benawv@gmail.com>
2014-03-28 08:26:17 +0700

    fix issue #169

--- application/models/campaign_model.php
+++ application/models/campaign_model.php
@@ -140,16 +140,18 @@ class Campaign_model extends CI_Model
 			$totalclicks = 0;
 			foreach($res->result() as $v)
 			{
-				$campaigns[$i]['short_urls'][$x]['content_campaign_url_id'] = $v->content_campaign_url_id;
-				$campaigns[$i]['short_urls'][$x]['long_url'] = $v->long_url;
-				$campaigns[$i]['short_urls'][$x]['short_code'] = $v->short_code;
-				$campaigns[$i]['short_urls'][$x]['description'] = $v->description;
-				$campaigns[$i]['short_urls'][$x]['increment'] = $v->increment;
-				$campaigns[$i]['short_urls'][$x]['created_at'] = date('M d, Y', strtotime($v->created_at));
-				$campaigns[$i]['short_urls'][$x]['display_name'] = $v->display_name;
-				$campaigns[$i]['short_urls'][$x]['qrcode_image'] = $v->qrcode_image;
-				$totalclicks = $totalclicks + $v->increment;
-				$x++;
+				if(isset($v->short_code)){
+					$campaigns[$i]['short_urls'][$x]['content_campaign_url_id'] = $v->content_campaign_url_id;
+					$campaigns[$i]['short_urls'][$x]['long_url'] = $v->long_url;
+					$campaigns[$i]['short_urls'][$x]['short_code'] = $v->short_code;
+					$campaigns[$i]['short_urls'][$x]['description'] = $v->description;
+					$campaigns[$i]['short_urls'][$x]['increment'] = $v->increment;
+					$campaigns[$i]['short_urls'][$x]['created_at'] = date('M d, Y', strtotime($v->created_at));
+					$campaigns[$i]['short_urls'][$x]['display_name'] = $v->display_name;
+					$campaigns[$i]['short_urls'][$x]['qrcode_image'] = $v->qrcode_image;
+					$totalclicks = $totalclicks + $v->increment;
+					$x++;
+				}
 			}
 			
 			$campaigns[$i]['total_clicks'] = $totalclicks;

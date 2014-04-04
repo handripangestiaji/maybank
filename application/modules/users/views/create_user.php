3304655555422b7fc493d1a952d870bd5446be1f
Eko Purnomo <eko.purnomo@icloud.com>
2014-04-04 15:44:35 +0700

    Issue #196 Fix

--- application/modules/users/views/create_user.php
+++ application/modules/users/views/create_user.php
@@ -67,11 +67,16 @@
                         <td>
                             <select name="optRole">
                                 <?php
-				foreach($role->result() as $r){
-					if($r->role_collection_id != $this->session->userdata('role_id') ||
-                                           (IsRoleFriendlyNameExist($this->user, 'User Management_User_Own_Country_Create'))) { ?>
-					<option value='<?php echo $r->role_collection_id;?>'><?php echo $r->role_name;?></option>
-                                <?php }}?>
+								foreach($role->result() as $r){
+									if(IsRoleFriendlyNameExist($this->user_role, 'User Management_User_All_Country_Create'))
+										echo '<option value="'.$r->role_collection_id.'">'.$r->role_name.'</option>';
+									else{
+										if($r->role_collection_id != $this->session->userdata('role_id')) 	
+											echo '<option value="'.$r->role_collection_id.'">'.$r->role_name.'</option>';
+									}
+								}				
+								?>
+                                
                             </select>
                         </td>
                     </tr>

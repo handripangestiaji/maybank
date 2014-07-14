2abeaa239bcf8735c5badcb7c0ff70766013e93a
Eko Purnomo <eko.purnomo@icloud.com>
2014-07-14 15:00:06 +0700

    Error Page revision...

--- application/errors/error_404.php
+++ application/errors/error_404.php
@@ -2,5 +2,5 @@
 
 
 $config =& get_config();
-echo curl_get_file_contents($config['base_url'].'login/error_page?heading='.$heading.'&content='.$message);
+echo curl_get_file_contents($config['base_url'].'login/error_page?heading='.$heading.'&user_id=xxxxx');
 
